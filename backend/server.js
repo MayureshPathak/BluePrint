ddeaf99cc3746a6f2ce005a1c108013926f503f5
MayureshPathak <mayureshpathak001@gmail.com>
2026-09-07 22:14:08 +0530

    local port availaible

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://10.240.230.230:5173',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 // Serve static files from the uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
